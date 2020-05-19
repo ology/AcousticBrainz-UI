d276833217e371492c5b0559c47d1f38278dd32c
Gene Boggs <gene@ology.net>
2020-05-18 21:14:32 -0700

    Rollback url_encode

--- lib/Mojolicious/Plugin/Helper.pm
+++ lib/Mojolicious/Plugin/Helper.pm
@@ -23,11 +23,6 @@ sub register {
     my $self = shift;
     return scalar fix_latin(@_)
   });
-
-  $app->helper(url_encode => sub {
-    my $self = shift;
-    return url_escape(@_)
-  });
 }
 
 1;
