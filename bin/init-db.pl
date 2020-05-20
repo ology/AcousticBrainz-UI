32db0d5254764b4ae6f94092acf86ac5e7e3fd98
Gene Boggs <gene@ology.net>
2020-05-19 18:01:07 -0700

    Fix syntax error. SOB

--- bin/init-db.pl
+++ bin/init-db.pl
@@ -9,7 +9,7 @@ my $db_file = '/home/gene/Data/ab-low-level.db';
 
 unlink $db_file
     if -e $db_file;
-unlink $db_file . '.journal';
+unlink $db_file . '.journal'
     if -e $db_file . '.journal';
 
 my $schema = Schema->connect('dbi:SQLite:dbname=' . $db_file, '', '');
