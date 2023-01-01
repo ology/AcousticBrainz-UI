ee05b3982ca8c1d49efc152e6c086f54cb912441
Gene Boggs <gene@ology.net>
2022-12-31 22:00:41 -0800

    for?

--- bin/inspect-db.pl
+++ bin/inspect-db.pl
@@ -13,16 +13,12 @@ my $schema = Schema->connect('dbi:SQLite:dbname=' . $db_file, '', '');
 
 my $nf = Number::Format->new;
 
-for my $i (1 .. 2) {
-    my $artists = $schema->resultset('Artist')->count;
-    my $recordings = $schema->resultset('Recording')->count;
-    my $db_size = -s $db_file;
+my $artists = $schema->resultset('Artist')->count;
+my $recordings = $schema->resultset('Recording')->count;
+my $db_size = -s $db_file;
 
-    printf "Artists: %s; Recordings: %s; DB size: %s; Time: %s\n",
-        $nf->format_number($artists),
-        $nf->format_number($recordings),
-        $nf->format_number($db_size),
-        scalar(localtime);
-
-    sleep 5 unless $i == 2;
-}
+printf "Artists: %s; Recordings: %s; DB size: %s; Time: %s\n",
+    $nf->format_number($artists),
+    $nf->format_number($recordings),
+    $nf->format_number($db_size),
+    scalar(localtime);
