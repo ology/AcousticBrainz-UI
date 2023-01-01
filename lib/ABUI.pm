8ca12b9c5648734a022ea076767e3649f2f1a4c2
Gene Boggs <gene@ology.net>
2022-12-31 21:41:39 -0800

    Lose redundant comments

--- lib/ABUI.pm
+++ lib/ABUI.pm
@@ -1,22 +1,16 @@
 package ABUI;
 use Mojo::Base 'Mojolicious';
 
-# This method will run once at server start
 sub startup {
   my $self = shift;
 
-  # Load configuration from hash returned by config file
   my $config = $self->plugin('Config');
 
   $self->plugin('Helper');
 
-  # Configure the application
   $self->secrets($config->{secrets});
 
-  # Router
   my $r = $self->routes;
-
-  # Normal route to controller
   $r->get('/')->to('access#main');
   $r->get('/size')->to('access#size');
 }
